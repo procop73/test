9029936b39027e4eb77e327e8d2752c269c1b8f3
procop73 <bellfast@ukr.net>
2019-04-24 14:14:00 +0000

    modified:   main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Pasha111999999999999\n");
+  response.end("Hello Forever\n");
 });
 
 // listen on localhost:8000
